13776709de4b2ebf59aa85d71d9cc343ee19aa38
Mark Granoff <mark@granoff.net>
2016-10-15 10:48:44 -0400

    Removed NSObject unnecessary inheritance. Cleaned up class and method ACLs. Added documentation.

--- Strongbox/Classes/Strongbox.swift
+++ Strongbox/Classes/Strongbox.swift
@@ -9,30 +9,57 @@
 import Foundation
 import Security
 
-class Strongbox: NSObject {
+public class Strongbox {
     
     let keyPrefix: String
-    var lastStatus = errSecSuccess
+    public var lastStatus = errSecSuccess
     
-    override init() {
+    init() {
         if let bundleIdentifier = Bundle.main.bundleIdentifier {
             self.keyPrefix = bundleIdentifier
         } else {
             self.keyPrefix = ""
         }
-        super.init()
     }
     
     init(keyPrefix: String) {
         self.keyPrefix = keyPrefix
-        super.init()
     }
     
-    func archive(_ object: Any?, key: String) -> Bool {
+    /**
+     Insert an object into the keychain. Pass `nil` for object to remove it from the keychain.
+     
+     **Note:** This is a convenience method that calls `archive(_:key:accessibility)` passing
+     `kSecAttrAccessibleWhenUnlocked` for the last argument. If you require different keychain
+     accessibility, call the other method directly passing the accessibility leve you require.
+
+     - returns:
+        Boolean indicating success or failure
+     
+     - parameters:
+        - object: data to store. Pass `nil` to remove previous value for key
+        - key: key with which to associated stored value, or key to remove if `object` is nil
+     
+     */
+    public func archive(_ object: Any?, key: String) -> Bool
+    {
         return self.archive(object, key: key, accessibility: kSecAttrAccessibleWhenUnlocked)
     }
     
-    func archive(_ object: Any?, key: String, accessibility: CFString) -> Bool {
+    /**
+     Insert an object into the keychain. Pass `nil` for object to remove it from the keychain.
+     
+     - returns:
+        Boolean indicating success or failure
+     
+     - parameters:
+        - object: data to store. Pass `nil` to remove previous value for key
+        - key: key with which to associate the stored value, or key to remove if `object` is nil
+        - accessibility: keychain accessibility of item once stored
+     
+     */
+    public func archive(_ object: Any?, key: String, accessibility: CFString) -> Bool
+    {
         guard let _=object as? NSSecureCoding else {
             // The optional is empty, so remove the key
             
@@ -51,7 +78,17 @@ class Strongbox: NSObject {
         return self.set(data, key: key, accessibility: accessibility)
     }
     
-    func unarchive(objectForKey key:String) -> Any? {
+    /**
+     Retrieve an object from the keychain.
+     
+     - returns:
+        Re-constituted object from keychain, or nil if the key was not found. Since the method returns `Any?` it is
+        the caller's responsibility to cast the result to the type expected.
+     
+     - parameters:
+        - key: the key to use to locate the stored value
+    */
+    public func unarchive(objectForKey key:String) -> Any? {
         guard let data = self.data(forKey: key) else {
             return nil
         }
@@ -64,7 +101,7 @@ class Strongbox: NSObject {
 
     // MARK: Private functions to do all the work
     
-    internal func set(_ data: NSMutableData?, key: String, accessibility: CFString) -> Bool {
+    func set(_ data: NSMutableData?, key: String, accessibility: CFString) -> Bool {
         let hierKey = hierarchicalKey(key)
 
         let dict = service()
@@ -86,11 +123,11 @@ class Strongbox: NSObject {
         return lastStatus == errSecSuccess
     }
     
-    internal func hierarchicalKey(_ key: String) -> String {
+    func hierarchicalKey(_ key: String) -> String {
         return keyPrefix + "." + key
     }
     
-    internal func query() -> NSMutableDictionary {
+    func query() -> NSMutableDictionary {
         let query = NSMutableDictionary()
         query[kSecClass] = kSecClassGenericPassword
         query[kSecReturnData] = kCFBooleanTrue
@@ -98,13 +135,13 @@ class Strongbox: NSObject {
         return query
     }
     
-    internal func service() -> NSMutableDictionary {
+    func service() -> NSMutableDictionary {
         let dict = NSMutableDictionary()
         dict.setObject(kSecClassGenericPassword, forKey: kSecClass as! NSCopying)
         return dict
     }
     
-    internal func data(forKey key:String) -> Data? {
+    func data(forKey key:String) -> Data? {
         let hierKey = hierarchicalKey(key)
         let query = self.query()
         query.setObject(hierKey, forKey: kSecAttrService as! NSCopying)
@@ -112,7 +149,7 @@ class Strongbox: NSObject {
         var data: AnyObject?
         lastStatus = SecItemCopyMatching(query, &data)
         
-        return data as! Data?
+        return data as? Data
     }
 
 }
