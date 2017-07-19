4af8abd2232c2e6f3598499080d7e7181f46294f
Prince Ugwuh <prince.ugwuh@silvercar.com>
2017-07-19 16:34:45 -0500

    support for carthage

---
+++ Strongbox/Strongbox/Strongbox/Strongbox.h
@@ -0,0 +1,19 @@
+//
+//  Strongbox.h
+//  Strongbox
+//
+//  Created by Prince Ugwuh on 7/19/17.
+//  Copyright © 2017 Carthage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Strongbox.
+FOUNDATION_EXPORT double StrongboxVersionNumber;
+
+//! Project version string for Strongbox.
+FOUNDATION_EXPORT const unsigned char StrongboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Strongbox/PublicHeader.h>
+
+
