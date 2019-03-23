dba0bc99a9d3ea9c86c75aca682e16b920d682d0
Mark Granoff <mark@granoff.net>
2019-03-23 13:54:19 -0400

    Add a working example.

--- Example/Strongbox/ViewController.swift
+++ Example/Strongbox/ViewController.swift
@@ -7,18 +7,47 @@
 //
 
 import UIKit
+import Strongbox
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var fetchButton: UIButton!
+    @IBOutlet var fetchedValueLabel: UILabel! {
+        didSet {
+            fetchedValueLabel.text = nil
+        }
+    }
+    
+    struct Constants {
+        static let textFieldKey = "textFieldKey"
+    }
+    
+    private var strongbox = Strongbox()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textField.becomeFirstResponder()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func save() {
+        _ = strongbox.archive(textField.text, key: Constants.textFieldKey)
     }
-
+    
+    @IBAction func fetch() {
+        if let fetchedValue = strongbox.unarchive(objectForKey: Constants.textFieldKey) as? String {
+            DispatchQueue.main.async {
+                self.fetchedValueLabel.text = fetchedValue
+            }
+        }
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        save()
+        return true
+    }
+}
